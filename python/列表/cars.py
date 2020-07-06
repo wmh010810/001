811a2dbf9a2b35bc56267a91edf8d5a06575e0de
wmh010810 <62879930+wmh010810@users.noreply.github.com>
2020-07-07 00:26:40 +0800

    Add files via upload

---
+++ python/列表/cars.py
@@ -0,0 +1,11 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+print(sorted(cars))
+print(cars)
+cars.reverse()
+print(cars)
+print(len(cars))
+print(cars[-2])
---
+++ python/列表/player.py
@@ -0,0 +1,19 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+print(players[1:4])
+print(players[:4])
+print(players[2:])
+print(players[-3:])
+for player in players[:3]:
+    print(player.title())
+
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+print(my_foods)
+print(friend_foods)
+friend_foods = my_foods
+print(friend_foods)
+my_foods.append('面')
+print(friend_foods)
